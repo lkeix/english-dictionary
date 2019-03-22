a0d2b5e96b0b6868965d16477c1870d371121551
Keisuke Nakamura <kexgaku.at@gmail.com>
2019-03-22 14:01:52 +0900

    Initial Commit

---
+++ Englishdictionary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Englishdictionary
+//
+//  Created by 中村圭助 on 2019/03/22.
+//  Copyright © 2019年 中村圭助. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Englishdictionary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Englishdictionary
+//
+//  Created by 中村圭助 on 2019/03/22.
+//  Copyright © 2019年 中村圭助. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
